2a61d12a7ec44369fa2b7154f8e5f37199c00746
Panzerkommandant237 <buschrapperjack@gmx.de>
2025-02-22 13:46:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Beispiel für einfache JavaScript-Funktionen, die später erweitert werden können
+
+// Eine Funktion, die beim Laden der Seite eine Nachricht anzeigt
+window.onload = function() {
+    console.log("Panzer-Seite wurde geladen!");
+    alert("Willkommen auf der Panzer-Webseite!");
+}
+
